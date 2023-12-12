ae45367a7ee1cbc04fab32393f00512660c824e5
660610809-kornkawin <kornkawin_th@cmu.ac.th>
2023-12-12 17:01:42 +0700

    lab8-3-completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+int age,height,bounty;
+string a;
+cout<<"Enter your age: ";
+cin>>age;
+
+if(age<=25){
+cout<<"Enter your height: ";
+cin>>height;
+if(height<100){
+    a = "Chopper" ;
+}else if(height<180){
+    a = "Usopp";
+}else {
+    cout<<"Enter your bounty: ";
+    cin>>bounty;
+    a = "Sanji";
+    if(bounty>1100000000){
+        a = "Zoro";
+    }
+}
+}else if(age<=60){
+    cout<<"Enter your bounty: ";
+    cin>>bounty;
+    a = "Franky";
+    if(bounty>500000000){
+        a = "Jinbe";
+    }
+}else{
+    a = "Brook";
+}
+
+
+cout<<"Your character = "<<a;
+    return 0;
+}
